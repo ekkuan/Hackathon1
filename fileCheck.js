8869732125033ef4a5b513b8815d00819f6ef839
EC2 Default User <ec2-user@ip-172-31-10-197.us-west-1.compute.internal>
2017-01-25 02:35:48 +0000

    added CORS

--- fileCheck.js
+++ fileCheck.js
@@ -103,6 +103,12 @@ function updateFileToS3(imageFilePath) {
         });
 }
 
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.listen(3000, function() {
 	console.log('FileCheck app listening on port 3000!')
 })
