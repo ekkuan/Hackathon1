6aa33e15e905598d444ace84e255e14e84aa15ff
Eric Kuan <ekkuan@Erics-MacBook-Pro.local>
2017-01-24 21:34:30 -0800

    Removed stringify, doesn't work well.

--- fileCheck.js
+++ fileCheck.js
@@ -21,14 +21,12 @@ app.get('/list', function(req, res){
   };
   s3.listObjects(params,  function(err, data){    
     for(var i = 0; i < data.Contents.length; i++) {
-      var con = data.Contents[i];
+      data.Contents[i].Key ='//t//n'+data.Contents[i].Key+'\\n';
+      data.Contents[i].LastModified += '\\n';
+      data.Contents[i].ETag += '\\n';
+      data.Contents[i].Size += '\\n';
+      data.Contents[i].StorageClass += '\\n';
       data.Contents[i].Url = 'https://s3-us-west-1.amazonaws.com/' + data.Name + '/' + data.Contents[i].Key;
-      JSON.stringify({con.Key, con.LastModified, con.ETag, con.Size, con.StorageClass, con.Url}, null, '\t');
-      // data.Contents[i].Key ='//t//n'+data.Contents[i].Key+'\\n';
-      // data.Contents[i].LastModified += '\\n';
-      // data.Contents[i].ETag += '\\n';
-      // data.Contents[i].Size += '\\n';
-      // data.Contents[i].StorageClass += '\\n';
     }   
     res.send(data.Contents);
   })
